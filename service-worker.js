d8d10f64d2f91b132e6843d2cb87fa5f018c0061
Orgânic/Grup <124021536+Organic-Grup@users.noreply.github.com>
2026-04-21 19:25:50 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,23 @@
-let usuarios = JSON.parse(localStorage.getItem("usuarios")) || {};
 const CACHE_NAME = "cronograma-cache-v1";
+
+const FILES = [
+  "cronograma.html",
+  "manifest.json",
+  "icon.png"
+];
 
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "cronograma.html",
-        "manifest.json"
-      ]);
+      return cache.addAll(FILES);
     })
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
