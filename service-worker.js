804fd10afb9edb073f4f9c491750c05fc14d5ae9
Orgânic/Grup <124021536+Organic-Grup@users.noreply.github.com>
2026-04-21 17:05:09 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "cronograma-cache-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "cronograma.html",
+        "manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
